ee61599c34468f8efa74e6db9e0375d7cf8485b5
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-05-03 00:45:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+//The arithmetic operators you've been using, +, -, *, and /, are undoubtedly familiar to you.
+//The +, -, *, / are known as familiar operators in javascript.
+
+
+//====================Math operators===========================//
+
+//===ADDITION===//
+// var popularNumber = 8;
+// alert ( popularNumber );
+
+// var popularNumber = 4 + 4;
+// alert ( popularNumber );
+
+// var popularNumber = 4;
+// alert ( popularNumber + 4);
+
+// var firstNumber = 4;
+// var secondNumber = 4;
+// var popularNumber = firstNumber + secondNumber;
+// alert ( popularNumber );
+
+
+
+//===SUBTRACTION===//
+// var popularNumber = 0;
+// alert ( popularNumber );
+
+// var popularNumber = 2-2;
+// alert ( popularNumber );
+
+// var popularNumber = 2;
+// alert ( popularNumber - 2);
+
+// var firstNumber = 2;
+// var secondNumber = 2;
+// var popularNumber = firstNumber - secondNumber;
+// alert ( popularNumber );
+
+
+
+//===MULTIPLICATION===//
+// var popularNumber = 9;
+// alert ( popularNumber );
+
+// var popularNumber = 3*3;
+// alert ( popularNumber );
+
+// var popularNumber = 3;
+// alert ( popularNumber * 3 );
+
+// var firstNumber = 3;
+// var secondNumber = 3;
+// var popularNumber = firstNumber * secondNumber;
+// alert ( popularNumber );
+
+
+
+//===DIVISION===//
+// var popularNumber = 12;
+// alert ( popularNumber );
+
+// var popularNumber = 12 / 4;
+// alert ( popularNumber );
+
+// var popularNumber = 12;
+// alert ( popularNumber / 3 );
+
+// var firstNumber = 3;
+// var secondNumber = 3;
+// var popularNumber = firstNumber / secondNumber;
+// alert ( popularNumber );
+
+
+
+//===MODULOUS OPERATOD===//
+//It doesn't give you the result of dividing one number byanother. It gives you the remainder when the division is executed.
+
+// var popularNumber = 10 % 3;
+// alert ( popularNumber );
+
+// var popularNumber = 12;
+// alert ( popularNumber % 3 );
+
+// var firstNumber = 7;
+// var secondNumber = 3;
+// var popularNumber = firstNumber % secondNumber;
+// alert ( popularNumber );
